b2391d1fb225d9cc46a21d7378f97b88d5cf6656
Woody <nz.wgkim@gmail.com>
2023-01-09 16:28:49 +1300

    hiding the keys for firebase using .env

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,22 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmbKLL6ONeEAOZEbhK1AsJKt3OO_pCAhE",
-  authDomain: "nwitter-531c5.firebaseapp.com",
-  projectId: "nwitter-531c5",
-  storageBucket: "nwitter-531c5.appspot.com",
-  messagingSenderId: "179057114226",
-  appId: "1:179057114226:web:923aeacdb7f519883396eb",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmbKLL6ONeEAOZEbhK1AsJKt3OO_pCAhE",
+//   authDomain: "nwitter-531c5.firebaseapp.com",
+//   projectId: "nwitter-531c5",
+//   storageBucket: "nwitter-531c5.appspot.com",
+//   messagingSenderId: "179057114226",
+//   appId: "1:179057114226:web:923aeacdb7f519883396eb",
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
