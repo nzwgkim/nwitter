0f673563ee96f7bf690943a5eeee00e69574b586
Woody <nz.wgkim@gmail.com>
2023-01-09 16:07:40 +1300

    Completed the setup for Firebase!!!

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmbKLL6ONeEAOZEbhK1AsJKt3OO_pCAhE",
+  authDomain: "nwitter-531c5.firebaseapp.com",
+  projectId: "nwitter-531c5",
+  storageBucket: "nwitter-531c5.appspot.com",
+  messagingSenderId: "179057114226",
+  appId: "1:179057114226:web:923aeacdb7f519883396eb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
